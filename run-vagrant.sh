813fa1f986462aa063a982a37f0ba2a85c5c3c33
haruband <haruband@gmail.com>
2021-01-22 12:19:24 +0900

    Fix run-vagrant.sh

--- run-vagrant.sh
+++ run-vagrant.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-ansible-playbook -i inventory/vatech/hosts.vagrant.yaml -b -v --private-key $HOME/.vagrant.d/insecure_private_key --flush-cache $@
+INVENTORY=${1:-inventory/vatech/hosts.service.yml}
+PLAYBOOK=${2:-cluster.yml}
+
+ansible-playbook -i ${INVENTORY} -b -v --private-key $HOME/.vagrant.d/insecure_private_key --flush-cache ${PLAYBOOK}
